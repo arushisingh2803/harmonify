119d6cee4c90cd8d1d51ccda0b0de03e87a2c927
arushisingh2803 <c22359751@mytudublin.ie>
2026-02-24 22:55:14 +0000

    ui redesign for homepage

--- frontend/user/src/components/Homepage.tsx
+++ frontend/user/src/components/Homepage.tsx
@@ -1,12 +1,74 @@
+import "./style/Homepage.css";
+import { useEffect } from "react";
+
 export default function Homepage() {
   const loginSpotify = () => {
     window.location.href = "http://localhost:8000/login";
   };
-  // simple homepage with login button that redirects to Spoify 2.0 OAuth flow
+
+    useEffect(() => {
+    const items = document.querySelectorAll(".feature-item");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    items.forEach((item) => observer.observe(item));
+  }, []);
+
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Welcome to Harmonify</h1>
-      <button onClick={loginSpotify}>Login with Spotify</button>
+    <div className="homepage">
+
+        <div className="aura one"></div>
+        <div className="aura two"></div>
+        <div className="aura three"></div>
+        <div className="aura four"></div>
+        <div className="aura five"></div>
+        <div className="aura six"></div>
+
+      <section className="hero">
+        <h1 className="hero-title">harmonify.</h1>
+        <p className="hero-subtitle">
+          your music personified.
+        </p>
+        <button className="spotify-button" onClick={loginSpotify}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg"
+            alt="Spotify logo"
+            className="spotify-icon"
+          />
+          Login with Spotify
+        </button>
+      </section>
+
+      <section className="features">
+        <div className="feature-item">
+          <h2>your music persona</h2>
+          <p>decode the patterns behind your listening habits</p>
+        </div>
+        <div className="feature-item">
+          <h2>your sonic circle</h2>
+          <p>meet people who share your music taste.</p>
+        </div>
+        <div className="feature-item">
+          <h2>live your sound</h2>
+          <p>discover shows that match your favourite genres.</p>
+        </div>
+      </section>
+      <section className="features">
+
+    </section>
+
+      <footer className="footer">
+        <p>© 2026 Harmonify</p>
+      </footer>
     </div>
   );
 }
